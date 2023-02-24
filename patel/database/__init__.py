974f0862baa9883ecf77fbe838b2dcb4bf4e0a97
jagug <48366121+jagug@users.noreply.github.com>
2023-02-24 20:24:40 +0530

    Add files via upload

---
+++ patel/database/__init__.py
@@ -0,0 +1,9 @@
+"""EduuRobot database."""
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2018-2023 Amano LLC
+
+from typing import List
+
+from .core import database
+
+__all__: List[str] = ["database"]
